c3f11625cfef7201ba1b2f3967820bb56a042e19
tayuuki <yuuki.tatsu848@gmail.com>
2023-02-02 01:29:06 +0900

    itoaでけた

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,54 +1,55 @@
-// #include"libft.h"
+#include"libft.h"
 
-// char	*ft_strrev(char *str, int size, int minus)
-// {
-// 	int		i;
-// 	char	*tmp;
 
-// 	i = 0;
-// 	ft_strlcpy(tmp, str, size);
-// 	printf("\n%s\n", str);
-// 	if (!minus)
-// 		str[i++] = '-';
-// 	while (!tmp[i])
-// 	{
-// 		str[i] = tmp[size - i - 1];
-// 		i++;
-// 	}
-// 	return (str);
-// }
+char	*ft_strrev(char *tmp, int size, int minus)
+{
+	int		i;
+	char	*str;
 
-// char	*ft_itoa(int n)
-// {
-// 	char	*str;
-// 	char	*str_tmp;
-// 	int		minus;
+	i = 0;
+	str = (char *)malloc(sizeof(char) * (size + 1));
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, tmp, size+1);
+	if (minus)
+	{
+		str[i] = '-';
+		i++;
+	}
+	while (i < size)
+	{
+		str[i] = tmp[size - i - 1];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+char	*ft_itoa(int n)
+{
+	char	str[12];
+	int		i;
+	int		minus;
 
-// 	if (n == -2147483648)
-// 		return (ft_strdup("-2147483648"));
-// 	str = (char *)malloc(sizeof(char) * 12);
-// 	if (!str)
-// 		return (NULL);
-// 	ft_bzero(str, 12);
-// 	str_tmp = str;
-// 	minus = 1;
-// 	if (n == 0)
-// 		str[0] = '0';
-// 	if (n < 0)
-// 	{
-// 		minus = 0;
-// 		n *= -1;
-// 	}
-// 	while (n > 0)
-// 	{
-// 		printf("\n%s\n", str-1);
-// 		*str = (n % 10) + '0';
-// 		n /= 10;
-// 		str++;
-// 	}
-// 	// printf("\n%s\n", str_tmp);
-// return (ft_strrev(str_tmp, ft_strlen(str_tmp), minus));
-// }
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	ft_bzero(str, 12);
+	minus = 0;
+	if (n == 0)
+		str[0] = '0';
+	if (n < 0)
+	{
+		minus = 1;
+		n *= -1;
+	}
+	i = 0;
+	while (n > 0)
+	{
+		str[i] = (n % 10) + '0';
+		n /= 10;
+		i++;
+	}
+return (ft_strrev(str, ft_strlen(str)+minus, minus));
+}
 
 // int	main(void)
 // {
@@ -57,10 +58,15 @@
 // 	int n3 = -2147483648;
 // 	int n4 = 2147483647;
 // 	int n5 = 0;
-// 	printf("あ%s\n", ft_itoa(n1));
-// 	printf("あ%s\n", ft_itoa(n2));
-// 	printf("あ%s\n", ft_itoa(n3));
-// 	printf("あ%s\n", ft_itoa(n4));
-// 	printf("あ%s\n", ft_itoa(n5));
+// 	printf("%s\n", ft_itoa(n1));
+// 	printf("%s\n", ft_itoa(n2));
+// 	printf("%s\n", ft_itoa(n3));
+// 	printf("%s\n", ft_itoa(n4));
+// 	printf("%s\n", ft_itoa(n5));
+	// ft_itoa(n1);
+	// ft_itoa(n2);
+	// ft_itoa(n3);
+	// ft_itoa(n4);
+	// ft_itoa(n5);
 // 	return (0);
 // }
